063ab09d292b57cca775b6ce3e8a6e9ba716d658
iosdev <iosdev@labb509-17.exam.umn.ac.id>
2017-09-09 12:35:24 +0700

    ewrgeteeeeg5e5

---
+++ Week2_bisma/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  Week2_bisma
+//
+//  Created by iosdev on 9/9/17.
+//  Copyright © 2017 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
