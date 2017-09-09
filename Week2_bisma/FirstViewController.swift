e4be996fb764b292f7b6fc885195710a85fa011a
iosdev <iosdev@labb509-17.exam.umn.ac.id>
2017-09-09 09:03:05 +0700

    Initial Commit

---
+++ Week2_bisma/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Week2_bisma
+//
+//  Created by iosdev on 9/9/17.
+//  Copyright © 2017 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
